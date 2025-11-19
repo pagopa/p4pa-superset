0ded4f5a45231ceecb146af838fd1142eec04abe
antocalo <antonio.calo@nttdata.com>
2025-11-19 10:44:18 +0100

    P4ADEV-4165-implements-limitedUserInfo

--- src/configs/custom_security_manager.py
+++ src/configs/custom_security_manager.py
@@ -2,7 +2,6 @@ import logging
 import os
 import jwt
 from jwt import DecodeError
-from jwt.exceptions import InvalidAudience, InvalidSignature, ImmatureSignature
 from superset.security import SupersetSecurityManager
 from flask import flash
 import requests
